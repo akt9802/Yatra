4284955cd6a2be8320346545197fdb1bd6eaf86b
Aman Kumar <akt9802@gmail.com>
2025-08-13 10:30:00 +0530

    driver protector wrapper

--- frontend/src/pages/DriverProtectWrapper.jsx
+++ frontend/src/pages/DriverProtectWrapper.jsx
@@ -3,7 +3,6 @@ import { DriverDataContext } from "../context/DriverContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const DriverProtectWrapper = ({ children }) => {
-  //   const { user } = useContext(UserDataContext);
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
@@ -14,32 +13,27 @@ const DriverProtectWrapper = ({ children }) => {
     if (!token) {
       navigate("/driver-login");
     }
-  }, [token]);
 
-  axios
-    .get("${import.meta.env.VITE_BASE_URL}/drivers/profile", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        setDriver(response.data.driver);
-        setIsLoading(false);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      localStorage.removeItem("token");
-      navigate("/driver-login");
-    });
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/driver/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setDriver(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        localStorage.removeItem("token");
+        navigate("/captain-login");
+      });
+  }, [token]);
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
-  //   if (!token) {
-  //     navigate("/login");
-  //   }
 
   return <>{children}</>;
 };
