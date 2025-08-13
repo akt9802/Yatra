74963213f9e602db0afbc3d2282b921941133144
Aman Kumar <akt9802@gmail.com>
2025-08-13 10:14:21 +0530

    driver home socket implemented

--- frontend/src/pages/DriverHome.jsx
+++ frontend/src/pages/DriverHome.jsx
@@ -5,71 +5,123 @@ import RidePopUp from "../components/RidePopUp.jsx";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ConfirmRidePopUp from "../components/ConfirmRidePopUp.jsx";
+import { useEffect, useContext } from "react";
+import { SocketContext } from "../context/SocketContext";
+import { DriverDataContext } from "../context/CapatainContext";
+import axios from "axios";
 
-function DriverHome() {
+const DriverHome = () => {
+  const [ridePopupPanel, setRidePopupPanel] = useState(false);
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false);
 
-    const [ridePopupPanel,setRidePopupPanel] = useState(true);
-    const ridePopupPanelRef = useRef(null);
+  const ridePopupPanelRef = useRef(null);
+  const confirmRidePopupPanelRef = useRef(null);
+  const [ride, setRide] = useState(null);
 
-    const [confirmRidePopUpPanel, setConfirmRidePopUpPanel] = useState(false);
-    const confirmRidePopUpPanelRef = useRef(null)
+  const { socket } = useContext(SocketContext);
+  const { driver } = useContext(DriverDataContext);
 
-
-    
-    useGSAP(
-      function () {
-        if (ridePopupPanel) {
-          gsap.to(ridePopupPanelRef.current, {
-            transform: "translateY(0)",
+  useEffect(() => {
+    socket.emit("join", {
+      userId: driver._id,
+      userType: "driver",
+    });
+    const updateLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          socket.emit("update-location-captain", {
+            userId: driver._id,
+            location: {
+              ltd: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
           });
-        } else {
-          gsap.to(ridePopupPanelRef.current, {
-            transform: "translateY(100%)",
-          });
-        }
+        });
+      }
+    };
+
+    const locationInterval = setInterval(updateLocation, 10000);
+    updateLocation();
+
+    // return () => clearInterval(locationInterval)
+  }, []);
+
+  socket.on("new-ride", (data) => {
+    setRide(data);
+    setRidePopupPanel(true);
+  });
+
+  async function confirmRide() {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/rides/confirm`,
+      {
+        rideId: ride._id,
+        captainId: driver._id,
       },
-      [ridePopupPanel]
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
     );
 
-    useGSAP(
-      function () {
-        if (confirmRidePopUpPanel) {
-          gsap.to(confirmRidePopUpPanelRef.current, {
-            transform: "translateY(0)",
-          });
-        } else {
-          gsap.to(confirmRidePopUpPanelRef.current, {
-            transform: "translateY(100%)",
-          });
-        }
-      },
-      [confirmRidePopUpPanel]
-    );
+    setRidePopupPanel(false);
+    setConfirmRidePopupPanel(true);
+  }
+
+  useGSAP(
+    function () {
+      if (ridePopupPanel) {
+        gsap.to(ridePopupPanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(ridePopupPanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [ridePopupPanel]
+  );
+
+  useGSAP(
+    function () {
+      if (confirmRidePopupPanel) {
+        gsap.to(confirmRidePopupPanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(confirmRidePopupPanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [confirmRidePopupPanel]
+  );
 
   return (
     <div className="h-screen">
-      <div>
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
         <div
           style={{
             fontFamily: "poppins, sans-serif",
             fontWeight: 700,
-            fontSize: 30,
-            paddingLeft: 20,
-            paddingTop: 20,
+            fontSize: 35,
+            paddingLeft: 0,
+            paddingTop: 10,
             letterSpacing: "1px",
           }}
-          className="text-black absolute"
+          className="text-black"
         >
           <h3>Yatra</h3>
         </div>
         <Link
-          to="/driver-login"
-          className="fixed right-5 top-5 h-10 w-10 bg-white flex items-center justify-center rounded-full"
+          to="/driver-home"
+          className=" h-10 w-10 bg-white flex items-center justify-center rounded-full"
         >
           <i className="text-lg font-medium ri-logout-box-r-line"></i>
         </Link>
       </div>
-
       <div className="h-3/5">
         <img
           className="h-full w-full object-cover"
@@ -77,32 +129,32 @@ function DriverHome() {
           alt=""
         />
       </div>
-
       <div className="h-2/5 p-6">
-        <DriverDetails />
+        <CaptainDetails />
       </div>
-
       <div
         ref={ridePopupPanelRef}
-        className="fixed w-full z-10 translate-y-full bottom-0 bg-white px-3 py-10 pt-12"
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
       >
         <RidePopUp
-          setConfirmRidePopUpPanel={setConfirmRidePopUpPanel}
+          ride={ride}
           setRidePopupPanel={setRidePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+          confirmRide={confirmRide}
         />
       </div>
-
       <div
-        ref={confirmRidePopUpPanelRef}
-        className="fixed w-full h-screen z-10 translate-y-full bottom-0 bg-white px-3 py-10 pt-12"
+        ref={confirmRidePopupPanelRef}
+        className="fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
       >
         <ConfirmRidePopUp
-          setConfirmRidePopUpPanel={setConfirmRidePopUpPanel}
+          ride={ride}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
           setRidePopupPanel={setRidePopupPanel}
         />
       </div>
     </div>
   );
-}
+};
 
 export default DriverHome;
